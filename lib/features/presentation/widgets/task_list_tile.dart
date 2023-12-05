8eee714627b75ca49b6f0c7c0a37753874be6e40
Yehezkiel David Setiawan <yehezkieldavid2006@gmail.com>
2023-12-05 14:48:34 +0700

    aaa

--- lib/features/presentation/widgets/task_list_tile.dart
+++ lib/features/presentation/widgets/task_list_tile.dart
@@ -21,21 +21,46 @@ class TaskListTile extends StatelessWidget {
       ),
       trailing: Icon(Icons.arrow_forward),
       onTap: () {
-        _showDescriptionModal(context, task.title ?? "No Title available",
-            task.description ?? 'No description available');
+        _showDescriptionModal(
+            context,
+            task.title ?? "No Title available",
+            task.description ?? 'No description available',
+            task.imageUrl ?? "No Image available");
       },
     );
   }
 
   // Function to show the description modal
   void _showDescriptionModal(
-      BuildContext context, String title, String description) {
+      BuildContext context, String title, String description, String imageUrl) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text(title),
-          content: Text(description),
+          content: Container(
+            width: MediaQuery.of(context).size.width *
+                0.7, // Set modal width (adjust as needed)
+            child: Column(
+              children: [
+                // Add an image with rounded corners and 16:9 aspect ratio
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Image(
+                      image: NetworkImage(imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                    height:
+                        10), // Add some space between the image and description
+                Text(description),
+              ],
+            ),
+          ),
           actions: <Widget>[
             TextButton(
               onPressed: () {
